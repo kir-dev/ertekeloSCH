1973d32bafc1a8d7657203e13e0e122c9d17c2cd
EasySouls <tarjanyicsanad@gmail.com>
2024-05-03 17:05:49 +0200

    Prettier fix

--- apps/backend/src/metadata.ts
+++ apps/backend/src/metadata.ts
@@ -1,7 +1,44 @@
 /* eslint-disable */
 export default async () => {
-    const t = {
-        ["./users/entity/user.entity"]: await import("./users/entity/user.entity")
-    };
-    return { "@nestjs/swagger": { "models": [[import("./users/entity/user.entity"), { "User": { authSchId: { required: true, type: () => String }, email: { required: true, type: () => String }, name: { required: true, type: () => String }, role: { required: true, type: () => Object }, major: { required: true, type: () => Object }, desc: { required: true, type: () => String }, createdAt: { required: true, type: () => Date }, updatedAt: { required: true, type: () => Date } } }], [import("./users/dto/create-use.dto"), { "CreateUserDto": {} }], [import("./users/dto/update-user.dto"), { "UpdateUserDto": {} }]], "controllers": [[import("./app.controller"), { "AppController": { "getHello": { type: String } } }], [import("./users/users.controller"), { "UsersController": { "create": { type: t["./users/entity/user.entity"].User }, "findAll": { type: [t["./users/entity/user.entity"].User] }, "findOne": { type: t["./users/entity/user.entity"].User }, "update": { type: t["./users/entity/user.entity"].User }, "remove": { type: t["./users/entity/user.entity"].User } } }]] } };
+  const t = {
+    ['./users/entity/user.entity']: await import('./users/entity/user.entity'),
+  };
+  return {
+    '@nestjs/swagger': {
+      models: [
+        [
+          import('./users/entity/user.entity'),
+          {
+            User: {
+              authSchId: { required: true, type: () => String },
+              email: { required: true, type: () => String },
+              name: { required: true, type: () => String },
+              role: { required: true, type: () => Object },
+              major: { required: true, type: () => Object },
+              desc: { required: true, type: () => String },
+              createdAt: { required: true, type: () => Date },
+              updatedAt: { required: true, type: () => Date },
+            },
+          },
+        ],
+        [import('./users/dto/create-use.dto'), { CreateUserDto: {} }],
+        [import('./users/dto/update-user.dto'), { UpdateUserDto: {} }],
+      ],
+      controllers: [
+        [import('./app.controller'), { AppController: { getHello: { type: String } } }],
+        [
+          import('./users/users.controller'),
+          {
+            UsersController: {
+              create: { type: t['./users/entity/user.entity'].User },
+              findAll: { type: [t['./users/entity/user.entity'].User] },
+              findOne: { type: t['./users/entity/user.entity'].User },
+              update: { type: t['./users/entity/user.entity'].User },
+              remove: { type: t['./users/entity/user.entity'].User },
+            },
+          },
+        ],
+      ],
+    },
+  };
 };
